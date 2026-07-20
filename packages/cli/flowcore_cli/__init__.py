b4a6c0f96598bad61fae783bf56693dc42aef28b
rudran11 <rudranmurali2005@gmail.com>
2026-07-20 13:12:24 +0530

    chore(cli): bootstrap cli

---
+++ packages/cli/flowcore_cli/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2026 Rudran
+# Licensed under the MIT License.
+# See LICENSE file in the project root for full license information.
+
+"""
+FlowCore CLI Package.
+
+Provides local developer tooling for pipeline validation and management.
+"""
+
+__version__ = "1.0.0"
